10feb786cc29c75643601a18c406ac719339ff06
GonefishingX <Canas090@yahoo.com>
2016-11-03 18:36:33 -0700

    Glen's SpaceMan Push

---
+++ Assets/Scripts/ScaleOverTime.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleOverTime : MonoBehaviour {
+
+    private Vector3 StartingScale;
+    public Vector3 MaxScale;
+    public Vector3 ScalePerTick;
+    public float fScaleRate;
+
+    private float FTimertoNextScale;
+    private bool ScaleMet = false;
+    private Transform TargetToScale;
+
+	// Use this for initialization
+	void Start ()
+    {
+        TargetToScale = GetComponent<Transform>();
+        StartingScale = TargetToScale.localScale;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(ScaleMet == false)
+        {
+            FTimertoNextScale += Time.deltaTime;
+
+            if (FTimertoNextScale >= fScaleRate)
+            {
+                FTimertoNextScale = 0;
+                print("scale time");
+
+                TargetToScale.localScale += ScalePerTick;
+
+                if(TargetToScale.localScale.x >= ScalePerTick.x)
+                {
+                    ScaleMet = true;
+                }
+
+                /*
+                if (TargetToScale.localScale.x <= MaxScale.x)
+                {
+                    TargetToScale.localScale.Set(TargetToScale.localScale.x + ScalePerTick.x, TargetToScale.localScale.y, TargetToScale.localScale.z);
+                    print("scale x");
+                }
+                if (TargetToScale.localScale.y <= MaxScale.y)
+                {
+                    TargetToScale.localScale.Set(TargetToScale.localScale.x, TargetToScale.localScale.y + ScalePerTick.y, TargetToScale.localScale.z);
+                    print("scale y");
+                }
+                if (TargetToScale.localScale.z <= MaxScale.z)
+                {
+                    TargetToScale.localScale.Set(TargetToScale.localScale.x, TargetToScale.localScale.y, TargetToScale.localScale.z + ScalePerTick.z);
+                    print("scale z");
+                }
+                
+                else
+                {
+                    ScaleMet = true;
+                }*/
+            }
+        }
+	}
+}
