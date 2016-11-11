7935c4c77b37a22ef9627d7c05469d193eec2ca5
matty-lanouette <sierra.lanouette@digipen.edu>
2016-11-10 20:26:49 -0800

    aaaaa menu

--- Assets/Scripts/RobotSelectLogic.cs
+++ Assets/Scripts/RobotSelectLogic.cs
@@ -27,25 +27,30 @@ public class RobotSelectLogic : MonoBehaviour
 
     public PlayerIndex playerIndex;
 
+    public float MoveTimer;
+    private bool CanMove;
+
 	// Use this for initialization
 	void Start ()
     {
         SelectorTransform = GetComponent<RectTransform>();
+        CanMove = true;
+        MoveTimer = 10;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(!bDisabled)
+        if(!bDisabled && CanMove == true)
         {
             State = GamePad.GetState(playerIndex);
 
             KeyAxisH = State.ThumbSticks.Left.X;
             KeyAxisV = State.ThumbSticks.Left.Y;
 
-            { 
             if(KeyAxisH != 0)
             {
+                CanMove = false;
                 if (KeyAxisH > 0 && SelectorTransform.localPosition.x < 75)
                 {
                     SelectorTransform.localPosition += new Vector3(125, 0, 0);
@@ -58,6 +63,7 @@ public class RobotSelectLogic : MonoBehaviour
             }
             if(KeyAxisV != 0)
             {
+                CanMove = false;
                 if (KeyAxisV > 0 && SelectorTransform.localPosition.y < 170)
                 {
                     SelectorTransform.localPosition += new Vector3(0, 115, 0);
@@ -69,8 +75,17 @@ public class RobotSelectLogic : MonoBehaviour
             }
             
          }
+        if(CanMove == false)
+        {
+            --MoveTimer;
+            if(MoveTimer <= 0)
+            {
+                MoveTimer = 10;
+                CanMove = true;
+            }
+        }
 
-
+        //keyboard controls left in for testing -- matty
         if (PlayerNumber == 1)
         {
             if(Input.GetKeyDown(KeyCode.W) && SelectorTransform.localPosition.y < 170)
@@ -92,7 +107,7 @@ public class RobotSelectLogic : MonoBehaviour
 
         }
 
-
+        //keyboard controls left in for testing -- matty
         if (PlayerNumber == 2)
         {
             if (Input.GetKeyDown(KeyCode.UpArrow) && SelectorTransform.localPosition.y < 170)
@@ -112,12 +127,29 @@ public class RobotSelectLogic : MonoBehaviour
                 SelectorTransform.localPosition += new Vector3(-125, 0, 0);
             }
         }
+
+        //raycasting bois
+
+        Vector3 backRay = transform.TransformDirection(Vector3.back);
+        Debug.DrawRay(transform.position, backRay, Color.black);
+
+        if(Physics.Raycast(transform.position, backRay,300))
+        {
+            Debug.Log("shits been hit yo");
+        }
 	}
 
     public void OnHover()
     {
-        // check what player is doing the thing
+        //raycasting shit o h  b o y
 
+
+
+
+    }
+
+    public void OnSelect()
+    {
 
     }
 }
