d8b4ddc4a429acbd25cb5adf49b0bbb6ce612945
Stereob0y <linus.chan09@gmail.com>
2016-12-08 19:28:36 -0800

    More splash screen/start screen functionality and stuffs

---
+++ Assets/LoadOtherLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadOtherLevel : MonoBehaviour {
+
+	[SerializeField]
+	private string level;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void LoadLevel()
+	{
+		SceneManager.LoadScene (level);
+	}
+}
--- Assets/Scripts/SplashScreenController.cs
+++ Assets/Scripts/SplashScreenController.cs
@@ -10,17 +10,45 @@ public class SplashScreenController : MonoBehaviour {
 	private Animator TeamSplashScreenAnimator;
 	[SerializeField]
 	private Animator GameSplashScreenAnimator;
+	[SerializeField]
+	private Animator ControllerSplashScreenAnimator;
+
+	[SerializeField]
+	private Transform TeamSplashScreen;
+	[SerializeField]
+	private Transform ControllerSplashScreen;
+	[SerializeField]
+	private Transform DigipenSplashScreen;
+	[SerializeField]
+	private Transform GameSplashScreen;
 
 	// Use this for initialization
 	void Start () 
 	{
-		StartCoroutine (ActivateAnimation ());
+		StartCoroutine ("ActivateAnimation");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		//Interrupting the splash screens
+		if (Input.anyKeyDown) 
+		{
+			//Hiding unnecessary screens
+			DigipenSplashScreenAnimator.enabled = false;
+			TeamSplashScreenAnimator.enabled = false;
+			ControllerSplashScreenAnimator.enabled = false;
+
+			TeamSplashScreen.GetComponent<Image> ().enabled = false;
+			ControllerSplashScreen.GetComponent<Image> ().enabled = false;
+			DigipenSplashScreen.GetComponent<Image> ().enabled = false;
+
+			//Stopping the coroutine from continuing
+			StopCoroutine ("ActivateAnimation");
+
+			//Skipping to the start screen
+			StartCoroutine (ShowStartScreen());
+		}
 	}
 
 	IEnumerator ActivateAnimation()
@@ -41,12 +69,21 @@ public class SplashScreenController : MonoBehaviour {
 
 		yield return new WaitForSeconds (1f);
 
-		GameSplashScreenAnimator.SetBool ("Activate", true);
+		ControllerSplashScreenAnimator.SetBool ("Activate", true);
 
 		yield return new WaitForSeconds (5f);
 
-		GameSplashScreenAnimator.SetBool ("Activate", false);
+		ControllerSplashScreenAnimator.SetBool ("Activate", false);
 
 		yield return new WaitForSeconds (1f);
+
+		StartCoroutine(ShowStartScreen ());
+	}
+
+	IEnumerator ShowStartScreen()
+	{
+		GameSplashScreenAnimator.SetBool("Activate", true);
+
+		yield return new WaitForSeconds (100f);
 	}
 }
