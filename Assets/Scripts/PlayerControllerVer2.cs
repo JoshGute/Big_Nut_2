d307b49a17f0d5a7a7fafd4c42c679c78121c4c5
Linus Chan <linus.chan09@gmail.com>
2017-01-25 19:44:58 -0800

    more polished dash

--- Assets/Scripts/PlayerControllerVer2.cs
+++ Assets/Scripts/PlayerControllerVer2.cs
@@ -28,14 +28,21 @@ public class PlayerControllerVer2 : MonoBehaviour {
   //Dashing
   [SerializeField]
   private Transform dashTargetPos;
+
+  //Used to store an instance of the dash position to move towards
+  private Vector3 curDashTargetPos;
+
   [SerializeField]
   private int maxDashes = 1;
   [SerializeField]
   private int curDashes = 0;
   [SerializeField]
-  private float DashDistance = 5f;
-  [SerializeField]
   private float secDashRegenTime = 1f;
+
+  [SerializeField]
+  private float DashSpeed = 10f;
+
+  private bool isDashing = false;
 
   private bool RegenDash = false;
 
@@ -51,8 +58,13 @@ public class PlayerControllerVer2 : MonoBehaviour {
 
   private float regentime = 0f;
 
-	// Use this for initialization
-	void Start ()
+  ////Deprecated////
+  //[SerializeField]
+  //private float DashDistance = 5f;
+  ////Deprecated////
+
+  // Use this for initialization
+  void Start ()
   {
     curDashes = maxDashes;
 
@@ -60,24 +72,66 @@ public class PlayerControllerVer2 : MonoBehaviour {
     Tr = gameObject.GetComponent<Transform>();
 	}
 	
+
+  //Update is called once per frame.
   void Update ()
   {
-    if(RegenDash == true)
+
+    ////Dash logic////
+    if (Input.GetKeyDown(KeyCode.X))
+    {
+      if (curDashes > 0)
+      {
+        curDashes -= 1;
+
+        Dash();
+
+        if (curDashes < maxDashes)
+        {
+          RegenDash = true;
+        }
+      }
+
+      else
+      {
+        return;
+      }
+
+    }
+
+    if (RegenDash == true)
     {
       regentime += Time.deltaTime;
 
-      if (regentime >= secDashRegenTime)
+      if (regentime >= secDashRegenTime && curDashes < maxDashes)
       {
+        regentime = 0f;
         curDashes += 1;
+      }
+
+      else if (regentime >= secDashRegenTime && curDashes == maxDashes)
+      {
         //turn off regen
         RegenDash = false;
         //reset to 0
         regentime = 0f;
       }
     }
+
+    if(isDashing == true)
+    {
+      Tr.position = Vector3.MoveTowards(Tr.position, curDashTargetPos, DashSpeed);
+
+      if(Tr.position == curDashTargetPos)
+      {
+        isDashing = false;
+        curDashTargetPos = Vector3.zero;
+      }
+
+    }
   }
 
-	// Update is called once per frame
+	// Physics Update
 	void FixedUpdate ()
   {
     Vector3 ShipDirection = new Vector3(0f, 1f, 0f);
@@ -92,27 +146,6 @@ public class PlayerControllerVer2 : MonoBehaviour {
       Tr.Rotate(new Vector3(0f, 0f, 1f), -TurnSpeed);
     }
 
-    if(Input.GetKeyDown(KeyCode.X))
-    {
-      if(curDashes > 0)
-      {
-        curDashes -= 1;
-
-        Dash();
-
-        if (curDashes == 0)
-        {
-          RegenDash = true;
-        }
-      }
-
-      else
-      {
-        return;
-      }
-
-    }
-
     if (Input.GetKey(KeyCode.UpArrow))
     {
       Rb.AddRelativeForce(ShipDirection * Speed);
@@ -125,12 +158,21 @@ public class PlayerControllerVer2 : MonoBehaviour {
   }
 
   /* Dash Function
-   * Change the 'y' pos of the dash target if you want to change the pos. 
+   * Change the 'y' pos of the dash target's translation if you want to change the distance traveled.
   */
   void Dash ()
   {
+    //Instant Dash
+    /*
     Tr.position = dashTargetPos.position;
+    */
 
+    //Updating the position to dash towards;
+    curDashTargetPos = dashTargetPos.position;
+
+    isDashing = true;
+
+    ////Deprecated////
     //Imperfect, but ultimately not needed because I realized I could just parent a target object the desired
     // dashdistance away and use that as the target pos to dash to.
     /*
@@ -139,5 +181,6 @@ public class PlayerControllerVer2 : MonoBehaviour {
 
     Tr.position = DashPos;
     */
+    ////Deprecated////
   }
 }
