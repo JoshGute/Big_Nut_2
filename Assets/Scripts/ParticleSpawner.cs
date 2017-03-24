cb340feb59f006d68a37d14cca0b15cbf4e44abe
Linus Chan <linus.chan09@gmail.com>
2017-03-24 14:54:17 -0700

    Made a quick particle spawner

---
+++ Assets/Scripts/ParticleSpawner.cs
@@ -0,0 +1,79 @@
+﻿/*******************************  Ducks in a Row  *********************************
+Author: Linus 'why' Chan
+Contributors: 
+Course: GAM450
+Game:   Bolt Blitz
+Date:   3/24/2017
+File:   ParticleSpawner.cs
+
+Description:
+
+
+Current Problems:
+
+
+Copyright (C) 2017 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+******************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour {
+
+  //The prefab we want to spawn
+  [SerializeField]
+  GameObject Particle;
+
+  private float Spawntimer;
+
+  [SerializeField]
+  private float SpawnEveryXSeconds;
+
+  [SerializeField]
+  private bool SpawnParticleAutomatically = false;
+
+  bool SpawnParticle = false;
+
+	// Use this for initialization
+	void Start ()
+  {
+		if(SpawnParticleAutomatically == true)
+    {
+      SpawnParticle = true;
+    }
+    else
+    {
+      SpawnParticle = false;
+    }
+	}
+	
+  void SpawnParticles()
+  {
+    SpawnParticle = true;
+  }
+
+  void StopSpawningParticles()
+  {
+    SpawnParticle = false;
+  }
+
+	// Update is called once per frame
+	void Update ()
+  {
+    if (SpawnParticle == true)
+    {
+      Spawntimer += Time.deltaTime;
+
+      //Spawn the particle
+      if (Spawntimer > SpawnEveryXSeconds)
+      {
+        Instantiate(Particle, gameObject.transform);
+
+        Spawntimer = 0;
+      }
+    }
+	}
+}
