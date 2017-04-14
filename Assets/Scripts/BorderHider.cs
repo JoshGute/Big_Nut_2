7865d785417bf96977eeceae11be754691913e45
GonefishingX <Canas090@yahoo.com>
2017-04-13 22:58:27 -0700

    Fading Border

--- Assets/Scripts/BorderHider.cs
+++ Assets/Scripts/BorderHider.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class BorderHider : MonoBehaviour {
 
     public MeshRenderer ObjectToReveal;
+    private bool RevealObject = false;
+    private Vector4 MaterialColor;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        MaterialColor = ObjectToReveal.material.color;	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        if (RevealObject == false)
+        {
+            //print("1 " + RevealObject);
+            ObjectToReveal.material.color = new Vector4(MaterialColor.x, MaterialColor.y, MaterialColor.z, 0);
+        }	
+
+        if (RevealObject)
+        {
+            //print("2 " + RevealObject);
+            ObjectToReveal.material.color = new Vector4(MaterialColor.x, MaterialColor.y, MaterialColor.z, ObjectToReveal.material.color.a + 0.05f);
+        }
+
 	}
 
     void OnTriggerEnter(Collider col)
     {
-        ObjectToReveal.enabled = true;
+        if (col.GetComponent<PlayerControllerVer2>())
+        {
+            RevealObject = true;
+        }       
     }
 
     void OnTriggerExit(Collider col)
     {
-        ObjectToReveal.enabled = false;
+        if (col.GetComponent<PlayerControllerVer2>())
+        {
+            RevealObject = false;
+        }
     }
 }
