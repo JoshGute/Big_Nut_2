1cbfcb4a5cec0602f31140a6f8ae8b7f223fa51f
matty-lanouette <sierra.lanouette@digipen.edu>
2017-04-19 21:08:16 -0700

    polishednioqwedndqondc

---
+++ Assets/Scripts/HTPLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HTPLogic : MonoBehaviour
+{
+    private bool HTPOpen;
+
+    public Image HTPImage;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        HTPOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void HTPMenu()
+    {
+        if(HTPOpen == false)
+        {
+            HTPImage.GetComponent<Image>().enabled = true;
+            HTPOpen = true;
+        }
+        else if(HTPOpen == true)
+        {
+            HTPImage.GetComponent<Image>().enabled = false;
+            HTPOpen = false;
+        }
+    }
+}
