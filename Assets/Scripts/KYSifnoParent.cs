c093a6d5dceceffefcda4ad7ac2b0678d462ffaa
GonefishingX <Canas090@yahoo.com>
2017-04-19 20:51:47 -0700

    Bots Speeder and More Bullets

--- Assets/Scripts/KYSifnoParent.cs
+++ Assets/Scripts/KYSifnoParent.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class KYSifnoParent : MonoBehaviour
 {
 	// Update is called once per frame
+    public bool TimedDeath = false;
+    public float Timer = 2;
 	void Update ()
     {
 		if(GetComponentInParent<BulletScript>() == null)
         {
-            Destroy(gameObject);
+            if (TimedDeath)
+            {
+                Timer -= Time.deltaTime;
+                if (Timer <= 0)
+                {
+                    TimedDeath = false;
+                }
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
         }
 	}
 }
