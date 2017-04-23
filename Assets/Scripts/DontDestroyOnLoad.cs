db37103c4d1ba2fe6b3294280087ea4004a7d7bc
Linus Chan <linus.chan09@gmail.com>
2017-04-22 18:59:51 -0700

    Added comment code header things

--- Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -1,4 +1,23 @@
-﻿using System.Collections;
+﻿/*******************************  Ducks in a Row  *********************************
+Author: Matty Lanouette
+Contributors: 
+Course: GAM450
+Game:   Bolt Blitz
+Date:   4/22/2017
+File:   DontDestroyOnLoad.cs
+
+Description:
+
+
+Current Problems:
+
+
+Copyright (C) 2017 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+******************************************************************************/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/Scripts/SplashScreenController.cs
+++ Assets/Scripts/SplashScreenController.cs
@@ -1,4 +1,23 @@
-﻿using UnityEngine;
+﻿/*******************************  Ducks in a Row  *********************************
+Author: Matty Lanouette
+Contributors: 
+Course: GAM450
+Game:   Bolt Blitz
+Date:   4/22/2017
+File:   SplashScreenController.cs
+
+Description:
+
+
+Current Problems:
+
+
+Copyright (C) 2017 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+******************************************************************************/
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
